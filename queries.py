40e9ff36349d38339800e9fef5d9e6695fd63470
Caio Magalhães <69921724+ravencaio@users.noreply.github.com>
2025-09-09 18:39:33 +0000

    update

--- queries.py
+++ queries.py
@@ -1,6 +1,6 @@
 from db import conectar
 
-def adicionar_tarefa(nome,descricao)
+def adicionar_tarefa(nome,descricao):
     con = conectar()
     cur = con.cursor()
     cur.execute("INSERT INTO tarefas (nome, descricao) VALUES (?, ?)", (nome, descricao))
@@ -24,6 +24,6 @@ def adicionar_tarefa(nome,descricao)
 def atualizar_tarefa(nome,descricao):
     con = conectar()
     cur = con.cursor()
-    cur.execute("INSERT INTO tarefas (nome, descricao) VALUES (?, ?)", (nome, descricao))
+    cur.execute()
     con.commit()
     con.close()
