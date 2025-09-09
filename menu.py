e19490907354c3e1e83d655c77af9a9880f08266
Vívian Cunha <vivianfcsilva@gmail.com>
2025-09-09 16:34:05 +0000

    Atualização do menu

---
+++ menu.py
@@ -0,0 +1,13 @@
+# Criar menu interativo para o TaskCRUD
+
+def exibir_menu():
+    """Menu TaskCRUD"""
+    print('\n--- TaskCRUD - Gerenciador de Tarefas ---')
+    print('1. Nova tarefa (CREATE - C)')
+    print('2. Apresentar tarefas existentes (READ - R)')
+    print('3. Atualizar uma tarefa (UPDATE - U)')
+    print('4. Deletar uma tarefa (DELETE -D)')
+    print('5. Sair')
+    print('-----------------------------------------')
+    
+exibir_menu()
